ab3a9d9489c593e4a14bbfd03a7712488ba1b204
hoodcode <95625773+hoodcode@users.noreply.github.com>
2022-02-26 12:33:21 +0000

    Add files via upload

---
+++ cryptoApi_two.py
@@ -0,0 +1,24 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '74783d62-4aa2-4eb3-88c1-c6c45b44f5cc',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
